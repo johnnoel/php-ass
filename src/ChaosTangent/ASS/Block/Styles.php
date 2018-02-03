1a261b346a3ae8b313cb3af1f9a1c3376011b601
John Noel <chaostangent@gmail.com>
2018-02-03 12:17:47 +0000

    Updated license information across the board

--- src/ChaosTangent/ASS/Block/Styles.php
+++ src/ChaosTangent/ASS/Block/Styles.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the ChaosTangent PHP-ASS package
+ *
+ * (c) John Noel <john.noel@chaostangent.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code
+ */
+
 namespace ChaosTangent\ASS\Block;
 
 /**
